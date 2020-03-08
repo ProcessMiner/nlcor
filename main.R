3423b80db72d8593133509b9622421f6047cd4e8
cran2367 <nk.chitta.ranjan@gmail.com>
2020-03-08 17:55:06 -0400

    correlation line thickness and opacity added

--- main.R
+++ main.R
@@ -1,6 +1,7 @@
 install.packages("devtools")
 library(devtools)
 install_github("ProcessMiner/nlcor")
+devtools::load_all()
 
 library(nlcor)
 library(ggplot2)
@@ -15,5 +16,10 @@ print(nonlinear.cor)
 nonlinear.cor <- nlcor(x3, y3, plt = T)
 print(nonlinear.cor)
 
+# Refine argument
 nonlinear.cor <- nlcor(x3, y3, refine = 0.999, plt = T)
 print(nonlinear.cor)
+
+# Correlation line arguments
+nonlinear.cor <- nlcor(x1, y1, plt = T, line_thickness = 2.5, line_opacity = 0.8)
+print(nonlinear.cor$cor.plot)
